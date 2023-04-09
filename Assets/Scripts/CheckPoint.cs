4ced4f37411c7ad971ebcfc315b80e9a18520a6c
IlyaPlekhanov <i.plekhanov@greengreystudio.com>
2023-04-09 10:53:43 +0300

    test commit

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,7 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Точка возрождения
+/// </summary>
 public class CheckPoint : MonoBehaviour
 {
     public GameObject m_point;
